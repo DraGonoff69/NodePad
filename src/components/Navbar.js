9f837dde8f24f86831568d48198a820ebe5351cb
Ayush Mishra <100191388+DraGonoff69@users.noreply.github.com>
2023-11-10 18:48:48 +0530

    changes made

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,13 @@ function Navbar() {
           <Link to="/About" className="text-white">Notes</Link>
           <Link to="/SignUp" className="bg-white text-blue-500 px-3 py-2 rounded-full">Sign Up</Link>
           <Link to="/Login" className="bg-white text-blue-500 px-3 py-2 rounded-full">Login</Link>
+          <span className="relative inline-block">
+            <img className="h-10 w-10 rounded-full" src="https://overreacted.io/static/profile-pic-c715447ce38098828758e525a1128b87.jpg" alt="Dan_Abromov"/>
+            <span className="absolute bottom-0 right-0 block h-2.5 w-2.5 rounded-full bg-green-600 ring-2 ring-white"></span>
+          </span>
         </div>
+
+
       </nav>
     </div>
   )
